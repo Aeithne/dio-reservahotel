6d6e8d3efc0324b41a448be502a54c5235dea37e
Aeithne <40723680+Aeithne@users.noreply.github.com>
2024-09-05 18:06:53 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,26 @@
+using DioReservaHotel.Utils
+
+namespace DioReservaHotel {
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ... (código para criar uma lista de suítes, por exemplo)
+
+        while (true)
+        {
+            // ... (menu de opções: fazer reserva, listar reservas, sair)
+
+            switch (opcao)
+            {
+                case 1:
+                    // Solicitar dados do hóspede e da suíte
+                    // Criar um objeto Reserva e adicionar à lista de reservas
+                    break;
+                // ... outros casos
+            }
+        }
+    }
+}
+}
