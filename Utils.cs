10582d82f93fcbf623e5a91f48e7301d89d89744
Aeithne <40723680+Aeithne@users.noreply.github.com>
2024-09-05 20:06:28 -0300

    Update Utils.cs

--- Utils.cs
+++ Utils.cs
@@ -1,32 +1,48 @@
-namespace DioReservaHotel.Utils
-{
-public class Pessoa
-{
-    public string Nome { get; set; }
-}
+using System;
+using System.Collections.Generic;
 
-public class Suite
-{
-    public int Numero { get; set; }
-    public double ValorDiaria { get; set; }
-}
-
-public class Reserva
-{
-    public Pessoa Hospede { get; set; }
-    public Suite Suite { get; set; }
-    public DateTime DataEntrada { get; set; }
-    public DateTime DataSaida { get; set; }
-
-    public double CalcularValorTotal()
-    {
-        int dias = (DataSaida - DataEntrada).Days;
-        double valorTotal = dias * Suite.ValorDiaria;
-        if (dias > 10)
-        {
-            valorTotal *= 0.9; // Desconto de 10%
-        }
-        return valorTotal;
+namespace DioReservaHotel.Utils {
+  public class Pessoa {
+    public string Nome {
+      get;
+      set;
     }
-}
+  }
+
+  public class Suite {
+    public int Numero {
+      get;
+      set;
+    }
+    public double ValorDiaria {
+      get;
+      set;
+    }
+
+    public Suite(int numero, double valor) {
+      Numero = numero;
+      ValorDiaria = valor;
+    }
+  }
+
+  public class Reserva {
+    public Pessoa Hospede {
+      get;
+      set;
+    }
+    public Suite Suite {
+      get;
+      set;
+    }
+    public int Dias {
+      get;
+      set;
+    }
+
+    public double CalcularValorTotal(int totalDias, Suite suite) {
+      double valorTotal = totalDias * suite.ValorDiaria;
+      if (totalDias > 10) valorTotal *= 0.9; // Desconto de 10%
+      return valorTotal;
+    }
+  }
 }
