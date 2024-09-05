2e8b1328b52a317cabb87e48ab99d0a5dff7f2bb
Aeithne <40723680+Aeithne@users.noreply.github.com>
2024-09-05 18:02:15 -0300

    Create Utils.cs

---
+++ Utils.cs
@@ -0,0 +1,29 @@
+public class Pessoa
+{
+    public string Nome { get; set; }
+}
+
+public class Suite
+{
+    public int Numero { get; set; }
+    public double ValorDiaria { get; set; }
+}
+
+public class Reserva
+{
+    public Pessoa Hospede { get; set; }
+    public Suite Suite { get; set; }
+    public DateTime DataEntrada { get; set; }
+    public DateTime DataSaida { get; set; }
+
+    public double CalcularValorTotal()
+    {
+        int dias = (DataSaida - DataEntrada).Days;
+        double valorTotal = dias * Suite.ValorDiaria;
+        if (dias > 10)
+        {
+            valorTotal *= 0.9; // Desconto de 10%
+        }
+        return valorTotal;
+    }
+}
